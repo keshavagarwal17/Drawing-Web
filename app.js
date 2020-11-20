ab9b0b6f9950b60b43ba345c62bb0137aa14fffb
Keshav Agarwal <keshavagarwal1710@gmail.com>
2020-11-21 01:13:40 +0530

    Add functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementsByClassName("canvas")[0];
 const brushPic = document.getElementsByClassName('brush')[0];
+const eraserPic = document.getElementsByClassName('Eraser')[0];
 const colorblock = document.getElementsByClassName('colors')[0];
 var c1 = canvas.getContext('2d');
 
@@ -9,10 +10,17 @@ canvas.height = window.innerHeight-50;
 canvas.width = window.innerWidth;
 var x,y;
 let isDrawing = false;
+let eraser = false;
 canvas.addEventListener('mousedown',(event)=>{
     x = event.offsetX;
     y = event.offsetY;
     isDrawing = true;
+})
+
+eraserPic.addEventListener('click',(event)=>{
+    eraser = true;
+    eraserPic.style.borderBottom = "1px solid black";
+    brushPic.style.borderBottom = "none";
 })
 
 canvas.addEventListener('touchstart',(event)=>{
@@ -23,13 +31,22 @@ canvas.addEventListener('touchstart',(event)=>{
 
 
 canvas.addEventListener('mousemove',(event)=>{
-    if(isDrawing){
-    c1.moveTo(x,y);
-    c1.lineTo(event.offsetX,event.offsetY);
-    x = event.offsetX;
-    y = event.offsetY;
-    c1.strokeStyle= color;
-    c1.stroke();
+    if(isDrawing)
+    {
+        c1.beginPath();
+        if(!eraser){
+            c1.globalCompositeOperation = 'destination-over';
+            c1.lineWidth = 2;
+            c1.strokeStyle= color;
+        }else{
+            c1.globalCompositeOperation = 'destination-out';
+            c1.lineWidth = 10;
+        }
+        c1.moveTo(x,y);
+        c1.lineTo(event.offsetX,event.offsetY);
+        x = event.offsetX;
+        y = event.offsetY;
+        c1.stroke();
     }
 })
 
@@ -58,14 +75,15 @@ const changecolor = (p) =>{
 }
 
 brushPic.addEventListener("click",()=>{
+    eraser = false;
+    eraserPic.style.borderBottom = "none";
+    brushPic.style.borderBottom = "1px solid black";
     if(colorblock.style.opacity==1){
-        console.log("i am here")
-        console.log(colorblock.style.display)
         colorblock.style.opacity=0;
-        colorblock.style.maxHeight=null;
+        colorblock.style.transform= "translateX(220px)";
 
     }else{
         colorblock.style.opacity=1;
-        colorblock.style.maxHeight=colorblock.scrollHeight+'px';
+        colorblock.style.transform= "translateX(0px)";
     }
 })
